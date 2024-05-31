feb153e06da1b5530a338c8b894e96d61cb8f89e
Mikolaj Zychowicz <mikolaj.zychowicz@uj.edu.pl>
2024-05-31 18:05:11 +0200

    ...

--- tools/toolchain/scripts/generate_arch_files.sh
+++ tools/toolchain/scripts/generate_arch_files.sh
@@ -37,14 +37,14 @@ if [ "${with_intel}" != "__DONTUSE__" ]; then
   if [ "${TARGET_CPU}" = "native" ]; then
     BASEFLAGS="-fPIC -fp-model=precise -g -qopenmp -traceback -xHost -mavx5124fmaps"
   elif [ "${TARGET_CPU}" = "generic" ]; then
-    BASEFLAGS="-fPIC -fp-model=precise -g -mtune=${TARGET_CPU} -qopenmp -traceback -mavx5124fmaps"
+    BASEFLAGS="-fPIC -fp-model=precise -g -mtune=${TARGET_CPU} -qopenmp -traceback -mavx5124fmaps -mavx2"
   else
-    BASEFLAGS="-fPIC -fp-model=precise -g -march=${TARGET_CPU} -mtune=${TARGET_CPU} -qopenmp -traceback -mavx5124fmaps"
+    BASEFLAGS="-fPIC -fp-model=precise -g -march=${TARGET_CPU} -mtune=${TARGET_CPU} -qopenmp -traceback -mavx5124fmaps -mavx2"
   fi
-  OPT_FLAGS="-O2 -funroll-loops"
+  OPT_FLAGS="-O3 -funroll-loops"
   LDFLAGS_C="-nofor-main"
 else
-  BASEFLAGS="-fno-omit-frame-pointer -fopenmp -g -mtune=${TARGET_CPU} IF_ASAN(-fsanitize=address|) -mavx5124fmaps"
+  BASEFLAGS="-fno-omit-frame-pointer -fopenmp -g -mtune=${TARGET_CPU} IF_ASAN(-fsanitize=address|) -mavx5124fmaps -mavx2"
   OPT_FLAGS="-O3 -funroll-loops"
   LDFLAGS_C=""
 fi
